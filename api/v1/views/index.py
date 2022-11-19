f2bb98584c6a9db44254410c394956918a4435b5
Aisosa <allysonaghedo@gmail.com>
2022-11-19 17:47:16 +0100

    fixed pycodestyle errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,13 +10,13 @@ def status():
     """ route that returns a JSON status"""
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """ return object count"""
     return jsonify({"amenities": storage.count("Amenity"),
-    "cities": storage.count("City"),
-    "places": storage.count("Place"),
-    "reviews": storage.count("Review"),
-    "states": storage.count("State"),
-    "users": storage.count("User")
-    })
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})

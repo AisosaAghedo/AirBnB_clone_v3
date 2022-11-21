aaa9f47c8aebe4d4f7699a601cfbcd9bd9f4517f
Aisosa <allysonaghedo@gmail.com>
2022-11-21 10:54:46 +0100

    debugginggg

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -16,7 +16,7 @@ def getting_state():
     return jsonify(my_states)
 
 
-@app_views.route('/states/<string: state_id>', methods=["GET"], strict_slashes=False)
+@app_views.route('/states/<string:state_id>', methods=["GET"], strict_slashes=False)
 def state(state_id):
     """ Retrieves a State object """
     my_states = storage.get("State", state_id)
@@ -50,16 +50,16 @@ def post_states():
 
     return jsonify(new_state.to_dict()), 201
 
-@app_views.route('/states/<string: state_id>', methods=["PUT"], strict_slashes=False)
+@app_views.route('/states/<string:state_id>', methods=["PUT"], strict_slashes=False)
 def update_states(state_id):
     """Updates a State object"""
-    my_state = storage.get("State", state_id)
-    if my_state is None:
+    state = storage.get("State", state_id)
+    if state is None:
         abort(404)
     if not request.get_json():
-        return jsonify(error='Not a JSON'), 400
-    for key, val in request.get_json().items():
-        if key not in ['id', 'created_at', 'updated_at']:
-            setattr(my_state, key, val)
-    my_state.save()
-    return jsonify(my_state.to_dict())
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    for attr, val in request.get_json().items():
+        if attr not in ['id', 'created_at', 'updated_at']:
+            setattr(state, attr, val)
+    state.save()
+    return jsonify(state.to_dict())
